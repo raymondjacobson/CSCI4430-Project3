6b14fc8eacf9573033c26791d12855dd42c3bd76
Raymond Jacobson <raymondshujacobson8@gmail.com>
2014-05-02 03:43:07 -0400

    finish problem1

--- problem1-python/getStats.py
+++ problem1-python/getStats.py
@@ -7,9 +7,16 @@ if len(sys.argv) != 2:
   print "Error. Invoke: python getStats.py directory"
   exit(1);
 
+# Get base directory from sys argument
 base_directory = sys.argv[1]
+
+# Build list of subdirectory data & print
 sub_directories = traverse(base_directory)
-for k in sub_directories:
-  print k
-  print sub_directories[k]
-  print ""
+for directory in sub_directories:
+  print directory
+  print str(sub_directories[directory]['size']) + ' bytes'
+  print str(sub_directories[directory]['public']) + ' public'
+  print str(sub_directories[directory]['private']) + ' private'
+  print str(sub_directories[directory]['try']) + ' try'
+  print str(sub_directories[directory]['catch']) + ' catch'
+  print ''
--- problem1-python/lib.py
+++ problem1-python/lib.py
@@ -5,16 +5,38 @@ def traverse(path):
   returns array with nested sub directories"""
   subdirs = {}
   for root, dirs, files in os.walk(path):
-    subdirs[root] = getSubFiles(root)
+    subdirs[root] = getSubData(root)
   return subdirs
 
-
-def getSubFiles(directory):
+def getSubData(directory):
   """recursively gets all files in a directory (nested)
-  and returns a list of them"""
-  subfiles = []
+  and returns a list of them with data about keywords & sizes"""
+  subdata = {}
+  subdata['files'] = []
+  subdata['size'] = subdata['public'] = subdata['private'] = subdata['try'] = subdata['catch'] = 0
+
+  # Loop through nested directories
   for root, dirs, files in os.walk(directory):
-    for f in files:
-      if os.path.splitext(f)[1] == '.java':
-        subfiles.append(f)
-  return subfiles
+    # Loop through files in directory
+    for file in files:
+      # Filter on .java extension
+      if os.path.splitext(file)[1] == '.java':
+        subdata['files'].append(file)
+        file_name = root + '/' + file
+        subdata['size'] += os.path.getsize(file_name)
+
+        # Open current file and read contents into string
+        with open(file_name, 'r') as my_file:
+          file_data = my_file.read()
+
+        # Delete all instances of comments & string literals
+        file_data = re.sub('(\/\*([^*]|[\r\n]|(\*+([^*\/]|[\r\n])))*\*+\/|\/\/.*)', '', file_data)
+        file_data = re.sub('".*"', '', file_data)
+
+        # Calculate total occurences of java keywords
+        subdata['public'] += len(re.findall('((^|\s|;)public\s)', file_data))
+        subdata['private'] += len(re.findall('((^|\s|;)private\s)', file_data))
+        subdata['try'] += len(re.findall('((^|\s|;|})try(\s*{|{))', file_data))
+        subdata['catch'] += len(re.findall('(^|\s|;|})(catch\(|catch\s*\()', file_data))
+
+  return subdata
